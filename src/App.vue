6fe7620281ddaf19b1cc62b1bf779d780d16de7d
Yumshot <officialyumshot@gmail.com>
2023-02-21 14:34:10 -0800

    Sprite Creator Init

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+import Generator from './components/Generator.vue'
+</script>
+
+<template>
+  <html class="dark">
+  <div class="common-layout">
+    <el-container>
+      <el-card shadow="always">
+        <el-header>Halcyon Character Creator</el-header>
+        <el-container>
+          <el-aside width="400px">Character Viewer
+            <el-image style="width: 400px; height: 150px" lazy
+              src="https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg" fit="contain" />
+          </el-aside>
+          <el-main>
+            <Generator msg="Halcyon Character Creator" />
+          </el-main>
+        </el-container>
+      </el-card>
+    </el-container>
+  </div>
+
+  </html>
+</template>
+
+<style scoped></style>
---
+++ src/components/Generator.vue
@@ -0,0 +1,52 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+
+defineProps<{ msg: string }>()
+const gender = ref(0)
+const bodyPath = ref(`/assets/body/bodies/${gender.value === 0 ? 'male' : 'female'}/idle/`)
+const bodyString = bodyPath.value.toString()
+const bodyImages = import(`${bodyString}`)
+const headPath = ref(`/assets/head/human_${gender.value === 0 ? 'male' : 'female'}/idle/`)
+const hairPath = ref('/assets/hair/')
+const eyePath = ref(`/assets/eyes/${gender.value === 0 ? 'male' : 'female'}/`)
+const bodyList = ref([]);
+const headList = ref([]);
+const hairList = ref([]);
+const eyeList = ref([]);
+
+
+console.log(await bodyImages)
+
+
+
+
+
+</script>
+
+<template>
+  <el-col :gutter="1">
+    Select a gender<br />
+    <el-space size="large" spacer="|"></el-space>
+    <el-radio-group v-model="gender">
+      <el-radio :label="0">Male</el-radio>
+      <el-radio :label="1">Female</el-radio>
+    </el-radio-group>
+  </el-col>
+  <el-row :gutter="12">
+    <el-col :span="6">
+
+    </el-col>
+  </el-row>
+</template>
+
+
+
+<style scoped>
+.bottom {
+  margin-top: 13px;
+  line-height: 12px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+</style>
